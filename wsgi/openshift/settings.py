2c4322bb7ea6ba40bfbfeea7710d4210c91fa952
Lorenzo <tunedconsulting@gmail.com>
2014-05-25 20:02:46 +0200

    Debug Started

--- wsgi/openshift/settings.py
+++ wsgi/openshift/settings.py
@@ -9,7 +9,7 @@ if 'OPENSHIFT_REPO_DIR' in os.environ:
 
 PROJECT_DIR = os.path.dirname(os.path.realpath(__file__))
 if ON_OPENSHIFT:
-    DEBUG = bool(os.environ.get('DEBUG', False))
+    DEBUG = bool(os.environ.get('DEBUG', True))
     if DEBUG:
         print("WARNING: The DEBUG environment is set to True.")
 else:
