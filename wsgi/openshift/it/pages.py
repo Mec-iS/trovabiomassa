64af26367e1d91fb4a54cf253f96e646f47cce5c
Lorenzo <tunedconsulting@gmail.com>
2014-06-17 10:34:20 +0200

    Pages View - Templates Corrected

--- wsgi/openshift/it/pages.py
+++ wsgi/openshift/it/pages.py
@@ -4,20 +4,20 @@ from django.views.decorators.csrf import ensure_csrf_cookie
 
 @ensure_csrf_cookie
 def privacy(request):
-    return render_to_response('home/privacy.html',
+    return render_to_response('it/home/privacy.html',
                               context_instance=RequestContext(request))
 
 @ensure_csrf_cookie
 def about(request):
-    return render_to_response('home/about.html',
+    return render_to_response('it/home/about.html',
                               context_instance=RequestContext(request))
 
 @ensure_csrf_cookie    
 def energia(request):
-    return render_to_response('home/energia.html',
+    return render_to_response('it/home/energia.html',
                               context_instance=RequestContext(request))
 
 @ensure_csrf_cookie
 def contacts(request):
-    return render_to_response('home/contacts.html',
+    return render_to_response('it/home/contacts.html',
                               context_instance=RequestContext(request))
