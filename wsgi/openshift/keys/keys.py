c80b5fe9bd65c2c5a4308654488ce3b4e01658e5
Lorenzo <tunedconsulting@gmail.com>
2015-07-18 11:36:33 +0200

    First commit on Github

--- wsgi/openshift/keys/keys.py
+++ wsgi/openshift/keys/keys.py
@@ -2,9 +2,9 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'info@trovabiomassa.com'
-EMAIL_HOST_PASSWORD = '1openStart2'
+EMAIL_HOST_PASSWORD = '**********'
 EMAIL_PORT = 587
 
 # captcha
-RECAPTCHA_PRIVATE_KEY = '6LdoXugSAAAAACixC2adzE9YDzzpJjtdB3P7qECg'
-RECAPTCHA_PUBLIC_KEY = '6LdoXugSAAAAAJDUDU9oeQGCuv8yrQ1GQGCPChBF'
+RECAPTCHA_PRIVATE_KEY = '************************'
+RECAPTCHA_PUBLIC_KEY = '*************************'
